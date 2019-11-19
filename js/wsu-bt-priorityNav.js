72cb96056848b6d23a26d1a464fa57542c533a14
Nic Ford <nicdford@gmail.com>
2019-11-18 17:04:03 -0800

    Setup initial priorityNav functionality

---
+++ js/wsu-bt-priorityNav.js
@@ -0,0 +1,36 @@
+"use strict";
+
+export default class wsu_bt_priorityNav {
+	constructor(mainNavSelector) {
+		this.mainNavSelector = mainNavSelector;
+		this.window = window;
+		this.document = document;
+
+		this.init();
+	}
+
+	// Methods
+	init() {
+		this.updateNav;
+		window.addEventListener('resize', this.updateNav.bind(this));
+	}
+
+	updateNav() {
+		if (this.screenWidth <= this.navWidth) {
+			console.log('we are going to do things here');
+		} else {
+			console.log('do nothing there is enough room for everyone to be happy');
+		}
+	}
+
+	// Getters
+	get screenWidth() {
+		const windowInnerWidth = this.window.innerWidth;
+		return windowInnerWidth;
+	}
+
+	get navWidth() {
+		const navInnerWidth = this.document.querySelector(this.mainNavSelector).offsetWidth;
+		return navInnerWidth;
+	}
+}
