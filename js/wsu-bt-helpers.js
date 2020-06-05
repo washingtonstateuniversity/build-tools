0e8372f11f75fd89bfcd043cbdc5afba7bb6814b
Nic Ford <nicdford@gmail.com>
2020-06-05 08:57:38 -0700

    Create repeater helper function and file

---
+++ js/wsu-bt-helpers.js
@@ -0,0 +1,15 @@
+
+/**
+ * Repeater.
+ * Repeats the markup within the repeater.
+ * @param {!number} props.numTimes Number of times the inner markup should be repeated.
+ */
+export function Repeater(props) {
+	let items = [];
+	
+	for (let i = 0; i < props.numTimes; i++) {
+	  items.push(props.children);
+	}
+
+	return items;
+}
