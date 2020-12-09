df3010843837f028f129f0dfd73d9690bf2b4ec0
Nic Ford <nicdford@gmail.com>
2020-12-09 13:21:17 -0800

    Move repeater helper function into helpers folder

--- js/helpers/repeater.js
+++ js/helpers/repeater.js
@@ -4,7 +4,7 @@
  * Repeats the markup within the repeater.
  * @param {!number} props.numTimes Number of times the inner markup should be repeated.
  */
-export function Repeater(props) {
+export default function Repeater(props) {
 	let items = [];
 	
 	for (let i = 0; i < props.numTimes; i++) {
